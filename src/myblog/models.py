930f83b278c4bab638da84c5a1144f5c7a8e875b
huseyin <hbayrak.hbayrak@gmail.com>
2021-01-13 15:59:19 +0200

    added models Posts and Categories

--- src/myblog/models.py
+++ src/myblog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    class Meta:
+        verbose_name_plural = "Categories" #admin panelinde class ismininin doğru yazımı için kod yazdık
+    def __str__(self): 
+        return self.name  #admin panelinde objelerin isim listesi olarakgözükmesiiçin kod yazdık
+    
+
+class Post(models.Model):
+    OPTIONS = (
+        ('d', 'Draft'),
+        ('p', 'Published')
+    )
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=OPTIONS, default='d')
+    slug = models.SlugField(blank=True)
+    def __str__(self): 
+        return self.name  #admin panelinde objelerin isim listesi olarakgözükmesiiçin kod yazdık
+    
+    
+
+    
--- src/myblog/views.py
+++ src/myblog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("HELLO WORLD")
+
+
+# def home(request):
+#     message = "hello world!"
+#     context = {
+#         "message": message
+#     }
+#     return render(request, "myblog/home.html", context)
