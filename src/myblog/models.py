a384f5296a7c8d0ba4267bf65dc31bc8a8e877ac
huseyin <hbayrak.hbayrak@gmail.com>
2021-01-18 13:12:26 +0200

    added like and  postview class

--- src/myblog/models.py
+++ src/myblog/models.py
@@ -34,5 +34,27 @@ class Post(models.Model):
         return self.title  #admin panelinde objelerin isim listesi olarakgözükmesiiçin kod yazdık
     
     
-
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
     
+    def __str__(self):
+        return self.user.username
+    
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delet=models.CASCADE)
+    
+    def __str__(self):
+        return self.user.username
+
+class PostView(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delet=models.CASCADE)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.user.username
