3adbcc724b66af69985b26cf60191bdf504cc2f4
huseyin <hbayrak.hbayrak@gmail.com>
2021-01-26 15:47:11 +0200

    added login logout

--- src/users/urls.py
+++ src/users/urls.py
@@ -5,9 +5,8 @@ from .views import register,profile
 # from .views import 
 
 urlpatterns = [
-    # path('login/', auth_views.LoginView.as_view, name='login'),# .as_view =>  parametreler eklenebilir
     path('register/',register, name='register'),
     path('profile/',profile, name='profile'),
-    # path('login/',auth_views.LoginView.as_view(), name='login'),
-   
+    path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"), # .as_view =>  parametreler eklenebilir
+    path("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"), # .as_view =>  parametreler eklenebilir
 ]
